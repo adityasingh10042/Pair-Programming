18d99d74b1d642c00898a5fe89fa6bb14dad7168
Aditya Singh <adityasingh10042@gmail.com>
2021-10-29 18:05:12 +0530

    Added sort array program in repo

---
+++ Arrays/Sort_array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define nl "\n"  
+#define ll long long   
+#define ull unsigned long long  
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);     
+const ull mod = 1e9+7; 
+
+int main(){
+ fast;
+ int n;
+ cin >> n;
+ int arr[n];
+ for(int i=0; i<n; i++)
+ {
+     cin >> arr[i];
+ }
+ sort(arr, arr+n);
+ cout << "Array after sorting is = ";
+ for(int i=0; i<n; i++)
+ {
+     cout << arr[i] << " ";
+ }
+ return 0;
+
+}  
