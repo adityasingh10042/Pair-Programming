2779e6321cffcf2fe43618f8b1a80be6426d9178
Aditya Singh <adityasingh10042@gmail.com>
2021-10-30 21:30:54 +0530

    Added Template code to the project

---
+++ Template Code/Code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+#include<algorithm>
+using namespace std; 
+#define nl "\n"  
+#define ll long long   
+#define ull unsigned long long  
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);     
+const ull mod = 1e9+7; 
+
+int main()
+{
+ fast;
+ return 0;
+
+}  
